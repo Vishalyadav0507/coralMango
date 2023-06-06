8e0e191733fb260eba9320b57d5111d823537244
Vishal yadav <120241696+Vishalyadav0507@users.noreply.github.com>
2023-06-06 15:40:03 +0530

    Add files via upload

--- es6.js
+++ es6.js
@@ -1,8 +1,9 @@
 // Given an array const a = [1, 3, 5, 2, 4] generate an array result1 from a, which should be equal to [1, 5, 4]. The solution should be of the form: 
 
-const a = [1, 3, 5, 2, 4] 
-let result1 = a.filter((num) => num % 2 !== 0);
-console.log(result1)
+const a = [1, 3, 5, 2, 4]
+const result = a.filter((element, index) => index % 2 === 0);
+
+console.log(result);
 
 // Given an array const a = [1, 3, 5, 2, 4]generate an array result2 from a, which should be equal to [1, 9, 25, 4, 16]. The solution should be of the form: 
 
@@ -11,5 +12,12 @@ console.log(result2)
 
 // Given an array const a = [1, 3, 5, 2, 4]generate an array result3 from a, which should be equal to [1, 25, 16]. The solution should be of the form: 
 
-const result3 = a.filter((num) => num % 2 !== 0).map((num) => num ** 2);
-console.log(result3)
+// const a = [1, 3, 5, 2, 4];
+const result3 = a.map((element, index) => {
+    if (index % 2 == 0) {
+        return element * element
+    }
+}).filter(element => element !== undefined);
+
+console.log(result3);
+
